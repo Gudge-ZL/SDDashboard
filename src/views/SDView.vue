30d7f989ebf315affd6506a5e9db23727ab097bb
Gudge-ZL <191870047@smail.nju.edu.cn>
2023-09-09 10:00:40 +0800

    urlConfigs

--- src/views/SDView.vue
+++ src/views/SDView.vue
@@ -1,10 +1,23 @@
 <template>
-    <iframe src="http://192.168.1.102:7860/?__theme=day" scrolling="false" style="width: 100%;height: 100%;">
+    <iframe id="SD" scrolling="false" style="width: 100%;height: 100%;">
     </iframe>
 </template>
 
 <script>
+import config from '../../config.json';
     export default {
         name: 'SDView',
+        mounted() {
+            this.setSrc();
+        },
+        methods: {
+            setSrc() {
+                const sdUrl = config.sdUrl;
+                const sdElement = document.getElementById('SD');
+                if (sdElement) {
+                    sdElement.src = `http://${sdUrl}/?__theme=day`;
+                }
+            }
+        }
     }
 </script>
